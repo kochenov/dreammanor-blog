ffcbb49a7dd3a302bb4803e9e393dc93245b7634
kochenov <kochenof@yandex.ru>
2023-01-27 17:51:31 +0300

    up

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ document.querySelector("#navbarSideCollapse").addEventListener("click", () => {
   document.querySelector(".offcanvas-collapse").classList.toggle("open");
 });
 
+
+
+let data;
+
 function getJson(id) {
   var xhr = new XMLHttpRequest();
 
@@ -17,6 +21,83 @@ function getJson(id) {
       findID(JSON.parse(this.responseText), id);
     }
   };
+}
+
+function getModalVideo(id) {
+  var xhr = new XMLHttpRequest();
+
+  // open a connection
+  xhr.open("GET", "./data.json", true);
+
+  // send the request
+  xhr.send();
+
+  // handle the response
+  xhr.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      //let arr = findIDVideo(, id);
+      data = JSON.parse(this.responseText);
+      let i = data.links.find((o) => o.id == id)
+      var modal = document.getElementById("exampleModalLabel");
+      var player = document.getElementById('player');
+      var findAd = document.getElementById('find-ad');
+      
+      modal.innerText = i.title;
+      player.innerHTML = `<iframe src="${i.video_zen}?from_block=partner&from=zen&backoffice=1&mute=1&autoplay=1&tv=0" allow="autoplay; fullscreen; accelerometer; gyroscope; picture-in-picture; encrypted-media" frameborder="0" scrolling="no" allowfullscreen=""></iframe>`;
+      findAd.innerHTML = `<button type="button"  id="find-ad" class="btn btn-primary" onClick="getJson(${i.id})">Найти объявление</button>`;
+      console.log(i);
+    }
+  };
+}
+
+function getJsonList() {
+  var xhr = new XMLHttpRequest();
+
+  // open a connection
+  xhr.open("GET", "./data.json", true);
+
+  // send the request
+  xhr.send();
+
+  // handle the response
+  xhr.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      let openEl = "";
+      var id = document.getElementById("list");
+      let arr = JSON.parse(this.responseText);
+      let str = "";
+
+      arr.links.forEach((el) => {
+        let video = "";
+        if (el.video_zen) {
+          video = el.video_zen;
+        } else {
+          video = el.video_rutube;
+        }
+        str =
+          str +
+          '<div class="d-flex text-muted pt-3"><p class="pb-3 mb-0 small lh-sm border-bottom"><strong class="d-block text-grey-dark"><a href="#" data-bs-toggle="modal" data-bs-target="#exampleModal" onClick="getVideo( ' +
+          el.id +
+          ')"> № ' +
+          el.id +
+          " : " +
+          el.title +
+          "</strong></a></p></div></div>";
+      });
+
+      id.innerHTML = str;
+    }
+  };
+}
+
+function getVideo(e) {
+  getModalVideo(e);
+}
+
+function findIDVideo(json, id) {
+  let data = json.links;
+  let res = data.find((o) => o.id == id);
+  return res;
 }
 
 function findID(json, id) {
@@ -48,3 +129,4 @@ if (id) {
   }
 }
 // window.location.replace("https://rutube.ru/channel/20022262/");
+getJsonList();
